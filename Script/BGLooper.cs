d0c386c9c26adecc574530257c5a9812f2e7599a
Yosua <iyoss.koyos@yahoo.com>
2016-10-21 18:16:00 +0700

    Add files via upload

---
+++ Script/BGLooper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	int numBGPanels = 2;
+
+	float treeMax = 0.8430938f;
+	float treeMin = -0.003243029f;
+
+	void Start() {
+		GameObject[] trees = GameObject.FindGameObjectsWithTag("Tree");
+
+		foreach(GameObject Tree in trees) {
+			Vector3 pos = Tree.transform.position;
+			pos.y = Random.Range(treeMin, treeMax);
+			Tree.transform.position = pos;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log ("Triggered: " + collider.name);
+
+		float widthOfBGObject = ((BoxCollider2D)collider).size.x;
+
+		Vector3 pos = collider.transform.position;
+
+		pos.x += widthOfBGObject * numBGPanels;
+
+		if(collider.tag == "Tree") {
+			pos.y = Random.Range(treeMin, treeMax);
+		}
+
+		collider.transform.position = pos;
+
+	}
+}
---
+++ Script/Movement.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	public float flapSpeed    = 100f;
+	public float forwardSpeed = 1f;
+
+	bool didFlap = false;
+
+	Animator animator;
+
+	public bool dead = false;
+	float deathCooldown;
+
+	public bool godMode = false;
+
+	// Use this for initialization
+	void Start () {
+		animator = transform.GetComponentInChildren<Animator>();
+
+		if(animator == null) {
+			Debug.LogError("Didn't find animator!");
+		}
+	}
+
+	// Do Graphic & Input updates here
+	void Update() {
+		if(dead) {
+			deathCooldown -= Time.deltaTime;
+
+			if(deathCooldown <= 0) {
+				if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) ) {
+					Application.LoadLevel( Application.loadedLevel );
+				}
+			}
+		}
+		else {
+			if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) ) {
+				didFlap = true;
+			}
+		}
+	}
+
+	
+	// Do physics engine updates here
+	void FixedUpdate () {
+
+		if(dead)
+			return;
+
+		GetComponent<Rigidbody2D>().AddForce( Vector2.right * forwardSpeed );
+
+		if(didFlap) {
+			GetComponent<Rigidbody2D>().AddForce( Vector2.up * flapSpeed );
+			animator.SetTrigger("DoFlap");
+
+
+			didFlap = false;
+		}
+
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(godMode)
+			return;
+
+		animator.SetTrigger("Death");
+		dead = true;
+		deathCooldown = 0.5f;
+	}
+}
